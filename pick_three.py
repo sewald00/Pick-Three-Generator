e57d7cbcbc2fa5f69d2e10ac13f83393100f8879
sewald00 <30478554+sewald00@users.noreply.github.com>
2018-02-19 11:17:51 -0700

    Update pick_three.py

--- pick_three.py
+++ pick_three.py
@@ -1,14 +1,16 @@
-                                            #Random pick three number generator
-                                            #Seth Ewald
-                                            #Feb. 16 2018
+"""Random pick three number generator
+   Seth Ewald
+   Feb. 16 2018"""
 
 import tkinter as tk
 from tkinter import ttk
 from random import randint
 
-                                            #Define the main program function to run
+
+#Define the main program function to run
 def main():
-                                            #Define function to change label text on button click
+  
+#Define function to change label text on button click
     def onClick():
         a = randint(0, 9)
         b = randint(0, 9)
@@ -17,18 +19,18 @@ def main():
         label.config(text=d)
 
 
-                                            #Create app window
+#Create app window
     root = tk.Tk()
     root.minsize(width=300, height=75)
     root.maxsize(width=300, height=75)
     root.title("Pick Three Generator")
 
-                                            #Create button to generate a random pick three
+#Create button to generate a random pick three
     button = tk.Button(root, text='Generate Numbers',
                        width=25, command=onClick)
     button.pack()
 
-                                            #Create a label to display the random pick three
+#Create a label to display the random pick three
     label = tk.Label(root, text='')
     label.pack()
 
